cc8d67f3bf78725ac4392383b39aeff953bf1dea
olena ivashyna <olena.ivashyna@gmail.com>
2013-06-21 14:17:19 -0700

    drop down menues on TodoLists

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,27 +18,62 @@ Todo.UI = (function() {
 		console.log(this.tasks);
 		this.makeMainDiv();
 		this.renderTasks();
-		//this.listenForClicks();
+		this.listenForClicks();
 	};
 	
 	UI.prototype.makeMainDiv = function(){
-		var div = $("<div class='main-div'>This is main div</div>")
+		var div = $("<div class='main-div'></div>")
+		var header = $('<h1>').html("Your Todo lists:");
+		div.prepend(header);
 		$('body').prepend(div);
 	}
 	
 	UI.prototype.renderTasks = function(){
 		var that = this;
 		var ol = $('<ol>');
-		console.log(that.tasks);
+
 		that.tasks.forEach(function(list, idx){
+			var div = $('<div>').attr({'class' : 'todo-list', 'id' : idx});
 			var listTitle = list['todo_list']['title'];
-			var a = $('<a>').attr({'href' : '#', 'id' : idx}).html(listTitle);
-			var li = $('<li>').html(a);
-			ol.append(li);
+			var a = $('<a>').attr({'href' : '#'}).html(listTitle);
+			var uniqID = "link" + idx;
+			var li = $('<li>').attr({'id' : uniqID}).html(a);
+			div.append(li);
+			ol.append(div);
 		});
 		$('.main-div').append(ol);
 	};
 	
+	UI.prototype.listenForClicks = function(){
+		var that = this;
+		$('.todo-list').click(function() {
+			var div = this;
+			var divOflistItems = that.listTodoItems($(div).attr('id'));
+			$(this).append(divOflistItems);
+		});
+	};
+	
+	UI.prototype.listTodoItems = function(divID) {
+		var div = $("id");
+		var newDiv = $('<div>');
+		var that = this;
+		var todoItemsArray = that.tasks[divID]['todo_list']['todo_items'];
+		var ul = $("<ul>");
+		todoItemsArray.forEach(function(item, idx) {
+			var li = $('<li>').html(item['name']);
+			$(ul).append(li);
+		});
+		$(newDiv).append(ul);
+		return newDiv;
+	}
 	
 	return UI;
 })();
+
+
+
+
+
+
+
+
